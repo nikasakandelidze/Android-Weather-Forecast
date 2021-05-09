9d0a8bf9524f62bc64d1855e607e5089842e7d2a
sakana <nikasakana@gmail.com>
2021-05-10 00:10:13 +0400

    added api call to weather RESt api

--- app/src/main/java/org/sakana/weatherforecast/apiAdapter/WeatherApiAdapter.kt
+++ app/src/main/java/org/sakana/weatherforecast/apiAdapter/WeatherApiAdapter.kt
@@ -1,8 +1,44 @@
 package org.sakana.weatherforecast.apiAdapter
 
+import com.google.gson.GsonBuilder
+import org.sakana.weatherforecast.apiAdapter.model.WeatherApiResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class WeatherApiAdapter {
     val api_key: String = "b792876327ea8f510d96527d72eba1a9"
+
+    public fun getCurrentWeather(): Unit {
+
+        val gson = GsonBuilder()
+            .setLenient()
+            .create();
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build();
+
+        val apiFetcher = retrofit.create<WeatherApiClient>(WeatherApiClient::class.java)
+
+        apiFetcher.geteCurrentWeatherData().enqueue(object : Callback<WeatherApiResponse> {
+            override fun onFailure(call: Call<WeatherApiResponse>, t: Throwable) {
+
+            }
+
+            override fun onResponse(
+                call: Call<WeatherApiResponse>,
+                response: Response<WeatherApiResponse>
+            ) {
+                if (response.isSuccessful) {
+                    //todo fill
+                }
+            }
+
+        })
+    }
 
 }
--- app/src/main/java/org/sakana/weatherforecast/apiAdapter/WeatherApiClient.kt
+++ app/src/main/java/org/sakana/weatherforecast/apiAdapter/WeatherApiClient.kt
@@ -5,6 +5,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface WeatherApiClient {
-    @GET("https://api.openweathermap.org/data/2.5/weather?q=Tbilisi&appid=b792876327ea8f510d96527d72eba1a9&units=metric")
-    fun currentData() : Call<WeatherApiResponse>
+    @GET("/data/2.5/weather?q=Tbilisi&appid={key}&units=metric")
+    fun geteCurrentWeatherData(): Call<WeatherApiResponse>
 }
